70e284eeeadc5257c755632f4163938034f48d7c
Ian Escarro <ian.escarro@gmail.com>
2020-05-24 08:53:28 +0800

    More update

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,6 +1,10 @@
 <?php
 
-class MY_Controller extends CI_Controller {
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . "third_party/MX/Controller.php";
+
+class MY_Controller extends MX_Controller {
 
   function __construct() {
     parent::__construct();
