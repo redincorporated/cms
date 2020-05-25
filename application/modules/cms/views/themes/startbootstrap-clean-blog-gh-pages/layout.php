08dce0b231ef198c7d57db8b89c247a70c72f3ea
Ian Escarro <ian.escarro@gmail.com>
2020-05-25 10:25:47 +0800

    Added base url

--- application/modules/cms/views/themes/startbootstrap-clean-blog-gh-pages/layout.php
+++ application/modules/cms/views/themes/startbootstrap-clean-blog-gh-pages/layout.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
   <head>
-
+    <base href="<?php echo base_url(); ?>">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
@@ -28,8 +28,6 @@
     <?php section('_header'); ?>
 
     <?php echo $content; ?>
-
-    <hr>
 
     <?php section('_footer'); ?>
 
